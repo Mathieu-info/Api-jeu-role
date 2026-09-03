0f7bf78f3af80cb548e81aaee49e1779219f9a57
KyattoKasai <sonicfrol@gmail.com>
2026-09-03 17:04:19 -0400

    Added Health class to manage entity health points

---
+++ General/Health.java
@@ -0,0 +1,30 @@
+public class Health {
+
+	private int current;
+	private int max;
+
+	public Health(int current, int max) {
+		if (current < 0 || current > max) {
+			throw new IllegalArgumentException(
+				"current must be between 0 and " + max
+			);
+		}
+		if (max <= 0) {
+			throw new IllegalArgumentException("max health must be positive");
+		}
+		this.current = current;
+		this.max = max;
+	}
+
+	public int getCurrent() {
+		return current;
+	}
+
+	public int getMax() {
+		return max;
+	}
+
+	public void takeDamage(int damage) {
+		current = Math.max(0, current - damage);
+	}
+}
