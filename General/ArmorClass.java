3310b75ef034839c27d9a53a251c8f350ea8ae8f
KyattoKasai <sonicfrol@gmail.com>
2026-09-03 17:09:36 -0400

    ArmorClass constructor input : value cannot be less than 0

--- General/ArmorClass.java
+++ General/ArmorClass.java
@@ -3,6 +3,11 @@ public class ArmorClass {
 	private int value;
 
 	public ArmorClass(int value) {
+		if (value < 0) {
+			throw new IllegalArgumentException(
+				"Armor class value cannot be negative"
+			);
+		}
 		this.value = value;
 	}
 
