410f6f009cb9e3159acd0bb3ddd9c939389252de
KyattoKasai <sonicfrol@gmail.com>
2026-09-03 17:43:08 -0400

    Implemented Enemy class structure

--- src/creatures/Enemy.java
+++ src/creatures/Enemy.java
@@ -1,1 +1,28 @@
-public class Enemy {}
+public class Enemy {
+
+	private Health health;
+	private EntityName name;
+	private ExperiencePoints exp;
+
+	public Enemy(Health health, EntityName name, ExperiencePoints exp) {
+		this.health = health;
+		this.name = name;
+		this.exp = exp;
+	}
+
+	public Health getHealth() {
+		return health;
+	}
+
+	public EntityName getName() {
+		return name;
+	}
+
+	public ExperiencePoints getExp() {
+		return exp;
+	}
+
+	public void takeDamage(int damage) {
+		health.takeDamage(damage);
+	}
+}
