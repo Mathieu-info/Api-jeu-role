0a78f26d4ce020ffc3dc33c28edb437183dcbda2
KyattoKasai <sonicfrol@gmail.com>
2026-09-03 17:42:55 -0400

    Added Enemy class and implemented EntityName validation

--- src/general/EntityName.java
+++ src/general/EntityName.java
@@ -1,1 +1,18 @@
-public class EntityName {}
+public class EntityName {
+
+	private String name;
+
+	public EntityName(String name) {
+		boolean containsNumbers = name.matches(".*\\d.*");
+		if (name == null || name.isEmpty() || containsNumbers == true) {
+			throw new IllegalArgumentException(
+				"name cannot be null or empty or contain numbers"
+			);
+		}
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
