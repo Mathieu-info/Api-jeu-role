379f1e48524db95550351ad0627504d702969df6
Gitihu10 <6243568@cegepmv.ca>
2026-09-25 12:51:38 -0400

    Classe Encounters.jaca

---
+++ src/Events/Encounters.java
@@ -0,0 +1,21 @@
+package Events;
+
+public class Encounters {
+    public static String[] encounters = {"Patrouille gobeline", "Campement orc", "Antre du Troll"};
+
+    public void randomEncounter() {
+        // nombre aléatoire entre 0 à nombre maximum de l'array
+        int encounter = (int) (Math.random() * encounters.length); // noombre aléatoire necessite le type int
+        // Condition qui appelle les méthodes necessaires
+        System.out.println("==========================");
+        System.out.println("Test de rencontre");
+        if (encounters[encounter].equals("Patrouille gobeline")) {
+            System.out.println("Patrouille gobeline");
+        } else if(encounters[encounter].equals("Campement orc")) {
+            System.out.println("Campement orc");
+        } else if(encounters[encounter].equals("Antre du Troll")) {
+            System.out.println("Antre du Troll");
+        }
+        System.out.println("==========================");
+    }
+}
